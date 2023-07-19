44fee4fb9f479fd27a181265538eff1b374ac96f
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-07-19 19:25:08 -0300

    a

--- aula12/ex13.js
+++ aula12/ex13.js
@@ -1,2 +1,10 @@
-var hora = 8
-console.log(`Agora são ex`)
+var agora = new Date();
+var hora = agora.getHours();
+var hora = console.log(`Agora são exatamente ${hora} horas.`);
+if (hora < 12) {
+  console.log("Bom dia!");
+} else if (hora < 18) {
+  console.log("Boa tarde!");
+} else {
+  console.log("Boa Noite");
+}
