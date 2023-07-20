4f488266a6104e088ba29054d9e40ce62224889f
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-07-20 04:15:31 -0300

    atualizar

---
+++ ex015/aula12ex/script.js
@@ -0,0 +1,24 @@
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    var min = data.getMinutes()
+    var seg = data.getSeconds()
+    // var hora = 18
+    msg.innerHTML = `<strong>Agora são ${hora}:${min}:${seg} horas.</strong>`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia
+        img.src = 'fotomanhã.png'
+        document.body.style.background = '#596448'
+    } else if (hora >= 12 && hora < 18) {
+        //Boa tarde
+        img.src = 'fototarde.png'
+        document.body.style.background = '#fcec55'
+    } else {
+        //Boa noite
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#001d43'
+    }
+
+}
