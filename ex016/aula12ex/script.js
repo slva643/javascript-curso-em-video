214b05c84724c047cf6014fe47cfcafa6da3d5be
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-07-20 16:31:09 -0300

    atualizar

---
+++ ex016/aula12ex/script.js
@@ -0,0 +1,8 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano)
+
+}
