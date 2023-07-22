e1b0ddd79c034fce711fee58b3d58ba677e4cdbe
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-07-22 15:15:15 -0300

    while

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+do {
+    console.log(`Passou ${c}`) // Estruta de execução no final do codigo
+    c++
+} while ((c <= 6) )
+
+/*
+    var c = 1
+    while (c <= 6) {
+    console.log(`Passou ${c}`) Estrutura de execução teste logico no inicio do codigo
+    c++
+}
+*/
