f3e5062ba4e60177cf72508a2ced4acd6b8f2136
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-07-21 22:52:34 -0300

    sd

--- ex016/aula12ex/script.js
+++ ex016/aula12ex/script.js
@@ -3,6 +3,40 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
     var res = window.document.querySelector('div#res')
-    if (fano.value.length == 0 || fano.value > ano)
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[erro] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gernero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 || idade < 10) {
+                //Criança
+            } else if (idade < 21) {
+                // jovem
+            } else if (idade < 50) {
+                // aduto
+            } else {
+                //idoso
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 || idade < 10) {
+                //Criança
+            } else if (idade < 21) {
+                // jovem
+            } else if (idade < 50) {
+                // aduto
+            } else {
+                //idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade}`
+
+    }
 
 }
